642b908a7e3ff27ca969deb81919148f8faa7723
Evan Gao <gaoevan37@gmail.com>
2023-10-02 09:23:37 -0400

    asdf

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 // declare and define function
 function test(mess) {
-    console.log("mess")
+    console.log(mess);
 }
-//outputs anything in the parameters
-test();
+
+test(true);
+
+const x = "String";
+
+test(x)
+
+const y = 98
+test(y);
+
+const z = true
+test(z)
+
+function add(x, y) {
+    console.log(x + y);
+}
+
+let a = Number(prompt("enter a number"));
+let b = Number(prompt("enter another number"));
+add(a, b);
+
+function add(x, y) {
+    return x + y;
+}
+
+const sum = add(4, 5);
+console.log(sum);
+
+function calc() {
+    const sum = add(4, 5);
+    console.log(sum)
+}
+
+calc();
